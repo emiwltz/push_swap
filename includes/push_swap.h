6c9665683194bcbdc7d79f6b5ac36ad6cac59773
Emilien Waltz <ewaltz@z2r2p3.42lyon.fr>
2026-01-21 12:47:36 +0100

    started to work on .h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,8 +6,34 @@
 /*   By: ewaltz <ewaltz@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 18:23:29 by ewaltz            #+#    #+#             */
-/*   Updated: 2026/01/20 18:24:59 by ewaltz           ###   ########.fr       */
+/*   Updated: 2026/01/21 12:46:41 by ewaltz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
 #include "libft.h"
+
+
+typedef struct s_node{
+  int	value;
+  int	rank;
+  struct s_node *next;
+}t_node;
+
+typedef enum e_algo{
+  ALGO_SIMPLE,
+  ALGO_MEDIUM,
+  ALGO_COMPLEX,
+  ALGO_ADAPTATIVE,
+}t_algo;
+
+typedef struct s_stack{
+  t_node *head;
+  t_node *tail;
+  int	size;
+}t_stack;
+
+#endif
+
