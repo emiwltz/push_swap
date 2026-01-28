2a9811aa057b12cc2a1b444ff7b8d5c60ba8fd26
emi <emilienwaltz@gmail.com>
2026-01-28 15:57:57 +0100

    added the new libft

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: ewaltz <ewaltz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 10:05:12 by ewaltz            #+#    #+#             */
-/*   Updated: 2026/01/22 12:14:18 by ewaltz           ###   ########.fr       */
+/*   Updated: 2026/01/20 15:26:05 by ewaltz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,5 +79,22 @@ char		*check_params(int fd);
 int			get_index(char *str);
 char		*get_rest(char *str, int start);
 char		*str_join(char *rest, char *buffer);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+						void (*del)(void *));
 
 #endif
