b8dc6c95d62991a5b2c2fcd625c35d06d87cf3aa
emi <emilienwaltz@gmail.com>
2026-02-19 11:38:52 +0100

    chunk_based fixed and opti

--- sorts/chunk_based.c
+++ sorts/chunk_based.c
@@ -6,7 +6,7 @@
 /*   By: alemyre <alemyre@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 13:18:47 by alemyre           #+#    #+#             */
-/*   Updated: 2026/02/17 16:50:14 by ewaltz           ###   ########.fr       */
+/*   Updated: 2026/02/19 11:34:43 by ewaltz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,24 +52,62 @@ static void	push_a_to_b(t_stack **a, t_stack **b, t_ctx *ctx, size_t chunk)
 	}
 }
 
+#include <stdio.h>
+
+static size_t	get_pos(t_stack **b)
+{
+  size_t	pos;
+  size_t	max;
+  t_node	*node;
+
+  pos = 0;
+  max = 0;
+  node = (*b)->head;
+  while (node != (*b)->tail)
+  {
+	if (node->rank > max)
+	  max = node->rank;
+	node = node->next;
+  }
+  if (node->rank > max)
+	  max = node->rank;
+  node = (*b)->head;
+  while (node->rank != max)
+  {
+	pos++;
+	node = node->next;
+  }
+  return (pos);
+}
+
+static	void	push_b_to_a(t_stack **a, t_stack **b, t_ctx *ctx, size_t pos)
+{
+  if (!b || !a)
+	return ;
+  if (pos < ((*b)->size / 2))
+  {
+	while ((*b) && (*b)->head->rank != (*b)->size)
+	  rb(b, ctx);
+	pa(b, a, ctx);
+  }
+  else
+  {
+	while ((*b) && (*b)->head->rank != (*b)->size)
+	  rrb(b, ctx);
+	pa(b, a, ctx);
+  }
+}
+
 void	chunk_based(t_stack **a, t_stack **b, t_ctx *ctx)
 {
-	size_t	chunk_size;
-	size_t	rank;
+  size_t	chunk_size;
 
-	if (!a || !(*a) || (*a)->size < 2)
-		return ;
-	chunk_size = get_sqrt((*a)->size);
-	push_a_to_b(a, b, ctx, chunk_size);
-	rank = (*b)->size;
-	while (*b && (*b)->size > 0)
-	{
-		if ((*b)->head->rank == rank)
-		{
-		  pa(b, a, ctx);
-		  rank--;
-		}
-		else
-		  rb(b, ctx);
-	}
+  if (!a || !(*a) || (*a)->size < 2)
+	return ;
+  chunk_size = get_sqrt((*a)->size);
+  push_a_to_b(a, b, ctx, chunk_size);
+  while (*b && (*b)->size > 0)
+  {
+	push_b_to_a(a, b, ctx, get_pos(b));
+  }
 }
